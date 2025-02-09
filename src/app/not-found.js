d40b89be95d94abb9e69516e7ec2fc015cbb579e
Alan Stubbs <alan@satowstubbs.co.uk>
2025-02-09 23:04:47 +0000

    Added 404 Metadata

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,6 +1,12 @@
 import Link from 'next/link';
 
 import styles from './not-found.module.css';
+import { BLOG_TITLE } from '@/constants';
+
+export const metadata = {
+    title: `404 Not Found • ${BLOG_TITLE}`,
+    description: '404 Not Found'
+}
 
 export default function NotFound() {
     return (
